5b686102127c1f71c6c3805e68aa77314360e722
AARON CHRIS DELOSO <deloso.aaronchris@auf.edu.ph>
2022-11-25 10:55:29 +0800

    Add project files.

---
+++ PDC60_MOD06/PDC60_MOD06/MainPage.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PDC60_MOD06
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetAddressCommand = new Command(async () => await OnGetAddress());
+            GetPositionCommand = new Command(async () => await OnGetPosition());
+            BindingContext = this;
+
+        }
+
+        //Defining variables
+
+        string lat = "15.1449853";
+        string lon = "120.5887029";
+        string address = "Santa Rita, Pampanga";
+        string geocodeAddress;
+        string geocodePosition;
+        public ICommand GetAddressCommand { get; }
+
+        public ICommand GetPositionCommand { get; }
+
+        public string Latitude
+        {
+            get => lat;
+            set => SetProperty(ref lat, value);
+        }
+
+        public string Longitude
+        {
+            get => lon;
+            set => SetProperty(ref lon, value);
+        }
+
+        public string GeocodeAddress
+        {
+            get => geocodeAddress;
+            set => SetProperty(ref geocodeAddress, value);
+        }
+
+        public string Address
+        {
+            get => address;
+            set => SetProperty(ref address, value);
+        }
+
+        public string GeocodePosition
+        {
+            get => geocodePosition;
+            set => SetProperty(ref geocodePosition, value);
+        }
+        //Function Get Position
+        async Task OnGetPosition()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+            try
+            {
+
+                var locations = await Geocoding.GetLocationsAsync(Address);
+                Location location = locations.FirstOrDefault();
+                if (location == null)
+                {
+                    GeocodePosition = "Unable to detect locations";
+                }
+                else
+                {
+                    GeocodePosition =
+                        $"{nameof(location.Latitude)}: {location.Latitude}\n" +
+                        $"{nameof(location.Longitude)}: {location.Longitude}\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                GeocodePosition = $"Unable to detect locations: {ex.Message}";
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        //Function Get Address
+        async Task OnGetAddress()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+            try
+            {
+                double.TryParse(lat, out var lt);
+                double.TryParse(lon, out var ln);
+
+                var placemarks = await Geocoding.GetPlacemarksAsync(lt, ln);
+                Placemark placemark = placemarks.FirstOrDefault();
+                if (placemark == null)
+                {
+                    GeocodeAddress = "Unable to detect placemarks.";
+                }
+                else
+                {
+                    GeocodeAddress =
+                        $"{nameof(placemark.AdminArea)}: {placemark.AdminArea}\n" +
+                        $"{nameof(placemark.Locality)}: {placemark.Locality}\n" +
+                        $"{nameof(placemark.CountryName)}: {placemark.CountryName}\n";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                GeocodeAddress = $"Unable to detect placemarks: {ex.Message}";
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        //Set Propertty
+        protected virtual bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "", Action onChanged = null, Func<T, T, bool> validateValue = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            if (validateValue != null && !validateValue(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
